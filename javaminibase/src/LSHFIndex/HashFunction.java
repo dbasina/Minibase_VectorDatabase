8e0e739c55d5f6d74bb1c03b1116fe2531616864
Meet Modi <meetmodi8@gmail.com>
2025-03-04 19:23:13 -0700

    Remove unused LSHFIndex classes and refactor Layer class to improve hash generation

--- javaminibase/src/LSHFIndex/HashFunction.java
+++ javaminibase/src/LSHFIndex/HashFunction.java
@@ -1,55 +1,54 @@
-package LSHFIndex;
+// package LSHFIndex;
 
-import global.Vector100Dtype;
+// import global.Vector100Dtype;
 
-import java.util.Random;
-import java.lang.Math.*;
+// import java.util.Random;
+// import java.lang.Math.*;
 
-/** The hash function is of the form
- * hash(input) = floor ((random.input + shift) / (bin_length))
- * random.input is dot product (projection) of the input onto a random plane.
- **/
-public class HashFunction
-{
-    private int shift;
-    private int bin_length;
-    private Vector100Dtype proj_vector;
-    private Random random;
+// /** The hash function is of the form
+//  * hash(input) = floor ((random.input + shift) / (bin_length))
+//  * random.input is dot product (projection) of the input onto a random plane.
+//  **/
+// public class HashFunction
+// {
+//     private int shift;
+//     private int bin_length;
+//     private Vector100Dtype proj_vector;
+//     private Random random;
 
 
-    public HashFunction(int bin_length)
-    {
+//     public HashFunction(int bin_length)
+//     {
+//         this.random = new Random();
 
-        this.random = new Random();
+//         this.bin_length = bin_length;
+//         this.shift = random.nextInt(bin_length);
 
-        this.bin_length = bin_length;
-        this.shift = random.nextInt(bin_length);
+//         // initialize the random vector to project onto
+//         this.proj_vector = new Vector100Dtype();
+//         int max = 10000;
+//         int min = -10000;
+//         for (int i = 0; i < 100; i++)
+//         {
+//             // This is just a trick to generate a random number in range [-10000, 10000]
+//             short randomNumber = (short) (random.nextInt(max - min + 1) + min);
+//             proj_vector.vector[i] = randomNumber;
+//         }
+//     }
 
-        // initialize the random vector to project onto
-        this.proj_vector = new Vector100Dtype();
-        int max = 10000;
-        int min = -10000;
-        for (int i = 0; i < 100; i++)
-        {
-            // This is just a trick to generate a random number in range [-10000, 10000]
-            short randomNumber = (short) (random.nextInt(max - min + 1) + min);
-            proj_vector.vector[i] = randomNumber;
-        }
-    }
+//     public int hash(Vector100Dtype input)
+//     {
+//         int dot_product = 0;
+//         int hash = 0;
+//         for (int i = 0; i < 100; i++)
+//         {
+//             dot_product += proj_vector.vector[i] * input.vector[i];
+//         }
 
-    public int hash(Vector100Dtype input)
-    {
-        int dot_product = 0;
-        int hash = 0;
-        for (int i = 0; i < 100; i++)
-        {
-            dot_product += proj_vector.vector[i] * input.vector[i];
-        }
+//         int numerator  = dot_product + shift;
+//         int denominator = bin_length;
+//         hash =  Math.floorDiv(numerator, denominator);
+//         return hash;
+//     }
 
-        int numerator  = dot_product + shift;
-        int denominator = bin_length;
-        hash =  Math.floorDiv(numerator, denominator);
-        return hash;
-    }
-
-}
+// }
--- javaminibase/src/LSHFIndex/LSHFIndex.java
+++ javaminibase/src/LSHFIndex/LSHFIndex.java
@@ -1,22 +1,27 @@
 package LSHFIndex;
 
 import global.AttrType;
-import global.PageId;
 import global.RID;
-import global.SystemDefs;
 import global.Vector100Dtype;
 import heap.HFBufMgrException;
 import heap.HFDiskMgrException;
 import heap.HFException;
 import heap.Heapfile;
-import heap.InvalidTypeException;
 import heap.Tuple;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import btree.IndexInsertRecException;
+import btree.IndexSearchException;
+import btree.InsertException;
+import btree.IteratorException;
+import btree.KeyTooLongException;
+import btree.LeafDeleteException;
+import btree.LeafInsertRecException;
+import btree.PinPageException;
+import btree.UnpinPageException;
 
 /**
  * LSHF index.
@@ -25,124 +30,70 @@ import java.util.Map;
  * Each Layer is a tree. Bunch of trees is LSHForest.
  * Each Layer = h1h2h3...hn where : hn = number of hashes per layer.
  */
-public class LSHFIndex
-{
-    private HashKey hashKey;
+public class LSHFIndex {
 
-    // LSHF parameters.
-    private int num_layers;
+    private int numLayers;
+    private int noOfHashFunctionsPerLayer;
 
-    // Layers parameters
-    private Layer[] layers;
-    private int bin_length;
-    private int num_hash_functions_per_layer;
-
-    private Map< Integer, Map<Integer[], Heapfile> > heapFiles;
-    private Map< Integer, Map<Integer[], Heapfile> > layer_maps;
+    private Layer[] layers; // L
+    private int binLength; // k
 
 
-    public LSHFIndex(int L, int x)
-    {
-        this.hashKey = new HashKey(L, x);
-        this.heapFiles = new HashMap<>();
+    /**
+     * Create LSHF Index class
+     *
+     * @param L No of layers. Input parameter.
+     * @param x No of bins. Input parameter.
+     * @param k No of hash functions per layer. Input parameter.
+    */
+    public LSHFIndex(int L, int x, int k) {
+        this.numLayers = L;
+        this.binLength = x;
+        this.noOfHashFunctionsPerLayer = k;
+        this.layers = new Layer[L];
+        for (int i = 0; i < L; i++) {
+            layers[i] = new Layer(noOfHashFunctionsPerLayer, binLength);
+        }
+    }
+
+    public String[] getAllLayersHash(Vector100Dtype vector) {
+        String[] hashValues = new String[numLayers];
+        for (int i = 0; i < numLayers; i++) {
+            hashValues[i] = layers[i].GetLayerHashAsString(vector);
+        }
+        return hashValues;
     }
 
     public void insertRecord(Vector100Dtype vector, RID rid) throws IOException,
-                                                                    ConstructPageException,
-                                                                    InsertRecException,
-                                                                    HFException,
-                                                                    HFBufMgrException,
-                                                                    HFDiskMgrException,
-                                                                    Exception
-    {
-        int[] hashValues = hashKey.generateHashValues(vector);
-        for (int layer = 0; layer < hashValues.length; layer++)
-        {
-            int hashValue = hashValues[layer];
-            Map<Integer, Heapfile> layerMap = heapFiles.get(layer);
-            if (layerMap == null)
-            {
-                layerMap = new HashMap<>();
-                heapFiles.put(layer, layerMap);
-            }
-            Heapfile heapFile = layerMap.get(hashValue);
-            if (heapFile == null)
-            {
-                String heapFileName = "layer-" + layer + "-bin-" + hashValue;
-                heapFile = new Heapfile(heapFileName); // Create a new heap file with a unique name
-                layerMap.put(hashValue, heapFile);
-            }
+            HFException,
+            HFBufMgrException,
+            HFDiskMgrException,
+            Exception {
+        String[] hashValues = getAllLayersHash(vector);
+        for (int layer = 0; layer < hashValues.length; layer++) {
+            String hashValue = hashValues[layer];
+            Heapfile heapFile = new Heapfile("layer-" + layer + "-bin-" + hashValue);
             insertRIDIntoHeapfile(heapFile, rid);
         }
     }
 
-    private void insertRIDIntoHeapfile(Heapfile heapFile, RID rid) throws IOException, InsertRecException, Exception
-    {
+    private void insertRIDIntoHeapfile(Heapfile heapFile, RID rid) throws IOException, Exception {
         Tuple tuple = new Tuple();
-        tuple.setHdr((short) 2,
-                new AttrType[]{new AttrType(AttrType.attrInteger), new AttrType(AttrType.attrInteger)}, null);
+        tuple.setHdr((short) 2, new AttrType[] { new AttrType(AttrType.attrInteger), new AttrType(AttrType.attrInteger) }, null);
         tuple.setIntFld(1, rid.pageNo.pid);
         tuple.setIntFld(2, rid.slotNo);
         heapFile.insertRecord(tuple.returnTupleByteArray());
     }
 
-    public List<String> getBins(Vector100Dtype vector) throws IOException, ConstructPageException
-    {
-        int[] hashValues = hashKey.generateHashValues(vector);
+    public List<String> getBinHeapFileNames(Vector100Dtype vector) throws IOException {
+
+        String[] hashValues = getAllLayersHash(vector);
         List<String> binNames = new ArrayList<>();
-        for (int layer = 0; layer < hashValues.length; layer++)
-        {
-            int hashValue = hashValues[layer];
-            Map<Integer, Heapfile> layerMap = heapFiles.get(layer);
-            if (layerMap != null)
-            {
-                Heapfile heapFile = layerMap.get(hashValue);
-                if (heapFile != null)
-                {
-                    String heapFileName = "layer-" + layer + "-bin-" + hashValue;
-                    binNames.add(heapFileName);
-                }
-            }
+        for (int layer = 0; layer < hashValues.length; layer++) {
+            String hashValue = hashValues[layer];
+                String heapFileName = "layer-" + layer + "-bin-" + hashValue;
+                binNames.add(heapFileName);
         }
         return binNames;
-    }
-
-    public static void main(String[] args)
-    {
-        try
-        {
-            // Initialize the system
-            String dbpath = "/tmp/minibase.lshfindex.test";
-            String logpath = "/tmp/minibase.lshfindex.log";
-            SystemDefs sysdef = new SystemDefs(dbpath, 1000, 100, "Clock");
-
-            // Create a new LSHFIndex
-            int L = 5; // Number of hash functions (layers)
-            int x = 10; // Number of bins
-            LSHFIndex lshfIndex = new LSHFIndex(L, x);
-
-            // Create a sample vector
-            Vector100Dtype vector = new Vector100Dtype();
-            for (int i = 0; i < 100; i++)
-            {
-                vector.vector[i] = (short) (i + 1);
-            }
-
-            // Insert the vector into the LSHFIndex
-            RID rid = new RID(new PageId(1), 1);
-            lshfIndex.insertRecord(vector, rid);
-
-            // Get the bin names for the vector
-            List<String> binNames = lshfIndex.getBins(vector);
-            System.out.println("Bin names: " + binNames);
-
-            // Clean up
-            SystemDefs.JavabaseDB.closeDB();
-
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
     }
 }
